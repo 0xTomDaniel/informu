812f258f73835b07bf08ec8abc8274ef985d01a8
Tom Daniel <tom.daniel@informu.io>
2019-11-15 12:23:45 -0700

    - Fixed Mu tag repo duplicating Mu tag instances with same ID

--- source/Secondary Adapters/Persistence/MuTagRepoRNCAsyncStorage.ts
+++ source/Secondary Adapters/Persistence/MuTagRepoRNCAsyncStorage.ts
@@ -6,58 +6,50 @@ import {
     FailedToRemove,
     FailedToUpdate,
 } from '../../Core/Ports/MuTagRepositoryLocal';
-import ProvisionedMuTag from '../../Core/Domain/ProvisionedMuTag';
+import ProvisionedMuTag, { BeaconID } from '../../Core/Domain/ProvisionedMuTag';
 import AsyncStorage from '@react-native-community/async-storage';
+
+interface PromiseExecutor {
+    resolve: (value?: void | PromiseLike<void> | undefined) => void;
+    reject: (reason?: any) => void;
+}
 
 export class MuTagRepoRNCAsyncStorage implements MuTagRepositoryLocal {
 
     private readonly muTagCache = new Map<string, ProvisionedMuTag>();
-    private muTagCacheHasAll = false;
+    private readonly muTagBeaconIDToUIDCache = new Map<string, string>();
+    private cachePopulated = false;
+    private readonly executeOnCachePopulated: PromiseExecutor[] = [];
+
+    constructor() {
+        this.populateCache();
+    }
 
     async getByUID(uid: string): Promise<ProvisionedMuTag> {
+        await this.onCachePopulated();
+
         if (this.muTagCache.has(uid)) {
             return this.muTagCache.get(uid) as ProvisionedMuTag;
+        } else {
+            throw new DoesNotExist(uid);
         }
+    }
 
-        let rawMuTag: string | null;
+    async getByBeaconID(beaconID: BeaconID): Promise<ProvisionedMuTag> {
+        await this.onCachePopulated();
 
-        try {
-            rawMuTag = await AsyncStorage.getItem(`muTags/${uid}`);
-        } catch (e) {
-            console.log(e);
-            throw new FailedToGet();
+        const uid = this.muTagBeaconIDToUIDCache.get(beaconID.toString());
+        if (uid != null && this.muTagCache.has(uid)) {
+            return this.muTagCache.get(uid) as ProvisionedMuTag;
+        } else {
+            throw new DoesNotExist(beaconID.toString());
         }
-
-        if (typeof rawMuTag !== 'string') {
-            throw new DoesNotExist();
-        }
-
-        const muTag = ProvisionedMuTag.deserialize(rawMuTag);
-        this.muTagCache.set(muTag.uid, muTag);
-        return muTag;
     }
 
     async getAll(): Promise<Set<ProvisionedMuTag>> {
-        if (this.muTagCacheHasAll) {
-            return new Set(this.muTagCache.values());
-        }
+        await this.onCachePopulated();
 
-        try {
-            const regExp = /^muTags\//;
-            const keys = await AsyncStorage.getAllKeys();
-            const muTagKeys = keys.filter((key): boolean => regExp.test(key));
-            const rawMuTagKeyPairs = await AsyncStorage.multiGet(muTagKeys);
-            const rawMuTags = rawMuTagKeyPairs
-                .map((rawMuTagKeyPair): string => rawMuTagKeyPair[1] || '')
-                .filter((rawMuTag): boolean => rawMuTag !== '');
-            const muTags = rawMuTags.map((rawMuTag): ProvisionedMuTag => ProvisionedMuTag.deserialize(rawMuTag));
-
-            muTags.forEach((muTag): void => { this.muTagCache.set(muTag.uid, muTag); });
-            return new Set(this.muTagCache.values());
-        } catch (e) {
-            console.log(e);
-            throw new FailedToGet();
-        }
+        return new Set(this.muTagCache.values());
     }
 
     async add(muTag: ProvisionedMuTag): Promise<void> {
@@ -66,6 +58,7 @@ export class MuTagRepoRNCAsyncStorage implements MuTagRepositoryLocal {
         try {
             await AsyncStorage.setItem(`muTags/${muTag.uid}`, rawMuTag);
             this.muTagCache.set(muTag.uid, muTag);
+            this.muTagBeaconIDToUIDCache.set(muTag.beaconID.toString(), muTag.uid);
         } catch (e) {
             console.log(e);
             throw new FailedToAdd();
@@ -95,9 +88,57 @@ export class MuTagRepoRNCAsyncStorage implements MuTagRepositoryLocal {
         try {
             await AsyncStorage.removeItem(`muTags/${uid}`);
             this.muTagCache.delete(uid);
+            const beaconIDs = [...this.muTagBeaconIDToUIDCache]
+                .filter(({ 1: value }): boolean => value === uid)
+                .map(([key]): string => key);
+            beaconIDs.forEach((key): void => { this.muTagBeaconIDToUIDCache.delete(key); });
         } catch (e) {
             console.log(e);
             throw new FailedToRemove();
         }
+    }
+
+    private async onCachePopulated(): Promise<void> {
+        if (this.cachePopulated) {
+            return;
+        }
+
+        return new Promise((resolve, reject): void => {
+            this.executeOnCachePopulated.push({ resolve: resolve, reject: reject });
+        });
+    }
+
+    private populateCache(): void {
+        this.getAllMuTagsFromDatabase().then((muTags): void => {
+            muTags.forEach((muTag): void => {
+                this.muTagCache.set(muTag.uid, muTag);
+                this.muTagBeaconIDToUIDCache.set(muTag.beaconID.toString(), muTag.uid);
+            });
+            this.cachePopulated = true;
+            this.executeOnCachePopulated.forEach((executor): void => {
+                executor.resolve();
+            });
+        }).catch((e): void => {
+            this.executeOnCachePopulated.forEach((executor): void => {
+                executor.reject(e);
+            });
+        }).finally((): void => {
+            this.executeOnCachePopulated.length = 0;
+        });
+    }
+
+    private async getAllMuTagsFromDatabase(): Promise<Set<ProvisionedMuTag>> {
+        const regExp = /^muTags\//;
+        const keys = await AsyncStorage.getAllKeys();
+        const muTagKeys = keys.filter((key): boolean => regExp.test(key));
+        const rawMuTagKeyPairs = await AsyncStorage.multiGet(muTagKeys);
+        const rawMuTags = rawMuTagKeyPairs
+            .map((rawMuTagKeyPair): string => rawMuTagKeyPair[1] || '')
+            .filter((rawMuTag): boolean => rawMuTag !== '');
+        const muTags = rawMuTags.map((rawMuTag): ProvisionedMuTag => {
+            return ProvisionedMuTag.deserialize(rawMuTag);
+        });
+
+        return new Set(muTags);
     }
 }
