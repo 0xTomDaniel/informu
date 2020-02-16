fc2d59f38e98356c41a2b0d1252a7ba5fe927da2
Tom Daniel <tom.daniel@informu.io>
2020-02-16 10:10:43 -0700

    Fixed console.log removal for babel

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,19 @@
-module.exports = {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
+module.exports = api => {
+    // This was modified to fix configurations for different build environments
+    // (https://github.com/babel/minify/issues/934)
+    const babelEnv = api.env();
+    const plugins = [
         ["@babel/plugin-proposal-decorators", { legacy: true }],
         ["transform-inline-environment-variables"]
-    ],
-    env: {
-        production: {
-            plugins: ["transform-remove-console"]
-        }
+    ];
+    if (babelEnv !== "development") {
+        plugins.push([
+            "transform-remove-console",
+            { exclude: ["error", "warn"] }
+        ]);
     }
+    return {
+        presets: ["module:metro-react-native-babel-preset"],
+        plugins
+    };
 };
