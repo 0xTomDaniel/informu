91c652a7c9a4b925990c279005ac07a34a6b4b7e
Tom Daniel <tom.daniel@informu.io>
2020-05-18 19:54:48 -0600

    Added current user location

--- source (restructure)/useCases/viewBelongingDashboard/presentation/BelongingDashboardViewController.tsx
+++ source (restructure)/useCases/viewBelongingDashboard/presentation/BelongingDashboardViewController.tsx
@@ -144,7 +144,7 @@ const styles = StyleSheet.create({
     },
     cardAddressView: {
         marginLeft: 58,
-        marginTop: -8,
+        marginTop: -7,
         flexDirection: "row"
     },
     cardAddressIcon: {
--- source (restructure)/useCases/viewBelongingMap/presentation/BelongingMapView.tsx
+++ source (restructure)/useCases/viewBelongingMap/presentation/BelongingMapView.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
+import { StyleSheet, Platform, StatusBar, View, StyleProp } from "react-native";
 import Theme from "../../../../source/Primary Adapters/Presentation/Theme";
 import DeviceInfo from "react-native-device-info";
 import { NavigationScreenProps, SafeAreaView } from "react-navigation";
@@ -11,9 +11,10 @@ import React, {
 import BelongingMapViewModel, {
     BelongingFeatureProperties
 } from "./BelongingMapViewModel";
-import MapboxGL from "@react-native-mapbox-gl/maps";
+import MapboxGL, { SymbolLayerStyle } from "@react-native-mapbox-gl/maps";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { FeatureCollection, Point } from "geojson";
+import { IconButton, FAB } from "react-native-paper";
 
 const styles = StyleSheet.create({
     safeAreaView: {
@@ -29,9 +30,24 @@ const styles = StyleSheet.create({
     map: {
         flex: 1
     },
-    annotation: {
-        backgroundColor: Theme.Color.PrimaryBlue
+    mapOverlayContainer: {
+        backgroundColor: "transparent",
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: 16,
+        paddingVertical: 24
+    },
+    fab: {
+        backgroundColor: "white"
     }
+    /*annotation: {
+        backgroundColor: Theme.Color.PrimaryBlue
+    }*/
 });
 
 interface BelongingMapViewProps extends NavigationScreenProps {
@@ -48,10 +64,13 @@ const BelongingMapView: FunctionComponent<BelongingMapViewProps> = (
         type: "FeatureCollection",
         features: []
     });
-    const [mapLocation, setMapLocation] = useState<number[]>([0, 0]);
     const [belongingMarkerIcon, setBelongingMarkerIcon] = useState<any>("");
+    const [mapLocation, setMapLocation] = useState<number[]>([0, 0]);
+    const [userLocation, setUserLocation] = useState<MapboxGL.Location>({
+        coords: { latitude: 0, longitude: 0 }
+    });
 
-    const mapboxStyles = {
+    const mapboxStyles: { [key: string]: StyleProp<SymbolLayerStyle> } = {
         belongingMarker: {
             iconImage: belongingMarkerIcon,
             iconAnchor: "bottom",
@@ -69,12 +88,12 @@ const BelongingMapView: FunctionComponent<BelongingMapViewProps> = (
         const subscription = props.belongingMapViewModel.showBelongingMarkers.subscribe(
             update => {
                 setBelongingMarkers(update);
-                if (update.features.length !== 0) {
+                /*if (update.features.length !== 0) {
                     setMapLocation([
                         update.features[0].geometry.coordinates[0],
                         update.features[0].geometry.coordinates[1]
                     ]);
-                }
+                }*/
             }
         );
 
@@ -95,12 +114,49 @@ const BelongingMapView: FunctionComponent<BelongingMapViewProps> = (
         ).then(source => setBelongingMarkerIcon(source));
     }, []);
 
+    const goToUserLocation = (): void => {
+        setMapLocation([
+            userLocation.coords.longitude,
+            userLocation.coords.latitude
+        ]);
+    };
+
+    const updateUserLocation = (location: MapboxGL.Location): void => {
+        const locationUpdateDelta =
+            location.timestamp == null
+                ? 2
+                : location.timestamp - (userLocation.timestamp ?? 0);
+        const didLongitudeChange =
+            location.coords.longitude - userLocation.coords.longitude !== 0;
+        const didLatitudeChange =
+            location.coords.latitude - userLocation.coords.latitude !== 0;
+        const didCoordinateChange = didLongitudeChange || didLatitudeChange;
+        if (locationUpdateDelta >= 2 && didCoordinateChange) {
+            const isFirstLocation =
+                userLocation.coords.longitude === 0 &&
+                userLocation.coords.latitude === 0;
+            setUserLocation(location);
+            if (isFirstLocation) {
+                setMapLocation([
+                    location.coords.longitude,
+                    location.coords.latitude
+                ]);
+            }
+        }
+    };
+
     return (
         <SafeAreaView style={[styles.safeAreaView, styles.base]}>
-            <MapboxGL.MapView style={styles.map} logoEnabled={false}>
+            <MapboxGL.MapView
+                style={styles.map}
+                logoEnabled={false}
+                attributionPosition={{ bottom: 24, left: 16 }}
+            >
+                <MapboxGL.UserLocation onUpdate={updateUserLocation} />
                 <MapboxGL.Camera
-                    zoomLevel={16}
+                    zoomLevel={17}
                     centerCoordinate={mapLocation}
+                    animationMode="flyTo"
                 />
                 <MapboxGL.ShapeSource id="belongings" shape={belongingMarkers}>
                     <MapboxGL.SymbolLayer
@@ -109,6 +165,14 @@ const BelongingMapView: FunctionComponent<BelongingMapViewProps> = (
                     />
                 </MapboxGL.ShapeSource>
             </MapboxGL.MapView>
+            <View pointerEvents="box-none" style={styles.mapOverlayContainer}>
+                <FAB
+                    icon="crosshairs-gps"
+                    color={Theme.Color.DarkGrey}
+                    onPress={goToUserLocation}
+                    style={styles.fab}
+                />
+            </View>
         </SafeAreaView>
     );
 };
