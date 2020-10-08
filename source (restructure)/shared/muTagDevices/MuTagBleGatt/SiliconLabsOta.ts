35ba58c6916396a0cf56d5a978780c212b6f47ce
Tom Daniel <tom.daniel@informu.io>
2020-10-08 11:53:26 -0600

    Updated class name casing.

--- source (restructure)/shared/muTagDevices/MuTagBleGatt/SiliconLabsOta.ts
+++ source (restructure)/shared/muTagDevices/MuTagBleGatt/SiliconLabsOta.ts
@@ -5,19 +5,19 @@ import {
 import Service from "./Service";
 import Hexadecimal from "../../metaLanguage/Hexadecimal";
 
-abstract class SiliconLabsOTAHexCharacteristic extends HexCharacteristic {
+abstract class SiliconLabsOtaHexCharacteristic extends HexCharacteristic {
     readonly serviceUuid = "1D14D6EE-FD63-4FA1-BFA4-8F47B42119F0";
 }
 
-class OTAControl extends SiliconLabsOTAHexCharacteristic
+class OtaControl extends SiliconLabsOtaHexCharacteristic
     implements WritableCharacteristic<Hexadecimal> {
     readonly uuid = "F7BF3564-FB6D-4E53-88A4-5E37E0326063";
     readonly byteLength = 1;
     readonly withResponse = true;
 }
 
-export default class SiliconLabsOTA extends Service {
+export default class SiliconLabsOta extends Service {
     static readonly uuid = "1D14D6EE-FD63-4FA1-BFA4-8F47B42119F0";
 
-    static readonly OTAControl = new OTAControl();
+    static readonly OtaControl = new OtaControl();
 }
