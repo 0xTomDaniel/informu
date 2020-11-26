4caee7ad6b9b84a031981665b84deba0d29b65f7
Tom Daniel <tom.daniel@informu.io>
2020-11-25 22:00:23 -0700

    Started writing AddMuTagViewModel tests.

--- source (restructure)/useCases/addMuTag/presentation/AddMuTagViewModel.test.ts
+++ source (restructure)/useCases/addMuTag/presentation/AddMuTagViewModel.test.ts
@@ -1,5 +1,26 @@
 import { AddMuTagViewModel } from "./AddMuTagViewModel";
 import { AddMuTagInteractor } from "../AddMuTagInteractor";
+import NavigationPort from "../../../shared/navigation/NavigationPort";
+
+/*const addMuTagRoutes = ["FindMuTag"] as const;
+const foobar = ["Foo", "Bar"] as const;
+const routes = [...addMuTagRoutes, ...foobar];
+type Routes = typeof routes[number];*/
+
+const routes = ["FindMuTag"] as const;
+type Routes = typeof routes[number];
+const navigationPortMocks = {
+    navigateTo: jest.fn<void, [Routes]>(),
+    popToTop: jest.fn<void, []>()
+};
+const NavigationPortMock = jest.fn<NavigationPort<Routes>, any>(
+    (): NavigationPort<Routes> => ({
+        routes: Object.assign({}, ...routes.map(v => ({ [v]: v }))),
+        navigateTo: navigationPortMocks.navigateTo,
+        popToTop: navigationPortMocks.popToTop
+    })
+);
+const navigationPortMock = new NavigationPortMock();
 
 const addMuTagInteractorMocks = {
     addFoundMuTag: jest.fn<Promise<void>, []>(),
@@ -17,9 +38,20 @@ const AddMuTagInteractorMock = jest.fn<AddMuTagInteractor, any>(
 );
 const addMuTagInteractorMock = new AddMuTagInteractorMock();
 
-const viewModel = new AddMuTagViewModel(addMuTagInteractorMock);
+const viewModel = new AddMuTagViewModel(
+    navigationPortMock,
+    addMuTagInteractorMock
+);
 
 test("Navigate to Find Mu Tag screen.", async () => {
-    expect.assertions(1);
+    expect.assertions(2);
     viewModel.goToFindMuTag();
+    expect(navigationPortMocks.navigateTo).toHaveBeenCalledTimes(1);
+    expect(navigationPortMocks.navigateTo).toHaveBeenCalledWith(
+        navigationPortMock.routes.FindMuTag
+    );
+});
+
+test("Start adding Mu tag.", async () => {
+    expect.assertions(2);
 });
--- source (restructure)/useCases/addMuTag/presentation/AddMuTagViewModel.ts
+++ source (restructure)/useCases/addMuTag/presentation/AddMuTagViewModel.ts
@@ -1,5 +1,6 @@
 import { Observable } from "rxjs";
 import { AddMuTagInteractor } from "../AddMuTagInteractor";
+import NavigationPort from "../../../shared/navigation/NavigationPort";
 
 // TODO: This should be part of a view model base class file.
 interface ViewModelUserMessage {
@@ -7,22 +8,33 @@ interface ViewModelUserMessage {
     details?: string;
 }
 
-export class AddMuTagViewModel {
+export const addMuTagViewModelRoutes = ["FindMuTag"] as const;
+
+type Routes = typeof addMuTagViewModelRoutes[number];
+
+export class AddMuTagViewModel<T extends string = never> {
     showActivity: Observable<boolean>;
     showError: Observable<ViewModelUserMessage | undefined>;
     showMessage: Observable<ViewModelUserMessage | undefined>;
 
-    constructor(addMuTagInteractor: AddMuTagInteractor) {
+    constructor(
+        navigation: NavigationPort<Routes | T>,
+        addMuTagInteractor: AddMuTagInteractor
+    ) {
         this.addMuTagInteractor = addMuTagInteractor;
+        this.navigation = navigation;
     }
 
     cancel(): void {}
 
-    goToFindMuTag(): void {}
+    goToFindMuTag(): void {
+        this.navigation.navigateTo("FindMuTag");
+    }
 
     setMuTagName(name: string): void {}
 
     startAddingMuTag(): void {}
 
     private addMuTagInteractor: AddMuTagInteractor;
+    private navigation: NavigationPort<Routes | T>;
 }
