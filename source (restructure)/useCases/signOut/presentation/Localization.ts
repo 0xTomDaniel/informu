9aef8bd5d41d848102a8a38c52e261dca303c158
Tom Daniel <tom.daniel@informu.io>
2021-01-20 13:55:18 -0700

    Completed sign out localization.

--- source (restructure)/useCases/signOut/presentation/Localization.ts
+++ source (restructure)/useCases/signOut/presentation/Localization.ts
@@ -1,7 +1,7 @@
 import * as D from "io-ts/lib/Decoder";
 
 export const SignOutTextDecoder = D.type({
-    error: D.type({
-        signOutFailed: D.string
+    BannerMessage: D.type({
+        SignOutFailed: D.string
     })
 });
